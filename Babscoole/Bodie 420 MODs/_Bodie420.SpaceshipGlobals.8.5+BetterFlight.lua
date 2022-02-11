274fb6878f7484fa2f1c670985572f375f450a61
DeathWrench <45341450+DeathWrench@users.noreply.github.com>
2022-02-11 18:29:46 -0500

    Update _Bodie420.SpaceshipGlobals.8.5+BetterFlight.lua

--- Babscoole/Bodie 420 MODs/_Bodie420.SpaceshipGlobals.8.5+BetterFlight.lua
+++ Babscoole/Bodie 420 MODs/_Bodie420.SpaceshipGlobals.8.5+BetterFlight.lua
@@ -17,49 +17,55 @@ NMS_MOD_DEFINITION_CONTAINER =
 						{
 							["VALUE_CHANGE_TABLE"] 	= 	
 							{
-								{"LandingMaxSpeed", "1000"},  --Original  80
-								{"MaximumDistanceFromShipWhenExiting", "2"},  --Original  10
-								{"MaximumHeightWhenExitingShip", "2"},  --Original  5
-								{"OutpostDockSpeedAlignMinDistance", "1"},  --Original  100
-								{"OutpostDockSpeedAlignRange", "1000"},  --Original  300
-								{"WarpInTimeFreighter", "0.1"},  --Original  1
-								{"WarpInFlashTimeFreighter", "0.1"},  --Original  0.98
-								{"WarpInTimeNexus", "0.1"},  --Original  1
-								{"WarpInFlashTimeNexus", "0.1"},  --Original  0.98
-								{"WarpNexusRotation", "0.1"},  --Original  15
-								{"WarpNexusPitch", "0.1"},  --Original  15
-								{"WarpNexusDistance", "-8000"},  --Original  -9000
-								{"LandingCheckBuildings", "false"},  --Original  true
-								{"LandingButtonMinTime", "0.05"},  --Original  0.5
+								{"LandingMaxSpeed", "1000"}, -- Original 300
+								{"MaximumDistanceFromShipWhenExiting", "2"}, -- Original 100
+								{"MaximumHeightWhenExitingShip", "2"}, -- Original 300
+								{"OutpostDockSpeedAlignMinDistance", "1"}, -- Original 100
+								{"OutpostDockSpeedAlignRange", "1000"}, -- Original 300
+								{"WarpInTimeFreighter", "0.1"}, -- Original 1
+								{"WarpInFlashTimeFreighter", "0.1"}, -- Original 0.98
+								{"WarpInTimeNexus", "0.1"}, -- Original 1
+								{"WarpInFlashTimeNexus", "0.1"}, -- Original 0.98
+								{"WarpNexusRotation", "1"}, -- Original 15
+								{"WarpNexusPitch", "1"}, -- Original 15
+								{"WarpNexusDistance", "-8000"}, -- Original -9000
+								{"LockTargetMinDistance", "1"}, -- Original 100
+								{"LandingCheckBuildings", "false"}, -- Original true
+								{"LandingButtonMinTime", "0.05"}, -- Original 0.5
+								{"LockTargetMinDistance", "1"}, -- Original 100
+								{"NoBoostStationDistance", "0.1"}, -- Original 2000
+								{"NoBoostAnomalyDistance", "0.1"}, -- Original 3000
+								{"NoBoostSpaceAnomalyDistance", "0.1"}, -- Original 3000
+								{"NoBoostFreighterDistance", "0.1"}, -- Original 800
+								{"NoBoostShipDistance", "0.1"}, -- Original 2000
+								{"NoBoostShipNearMinTime", "0.1"}, -- Original 5
+								{"NoBoostShipLastHitTime", "0.1"}, -- Original 10
+								{"NoBoostCombatEventMinBattleTime", "0.1"}, -- Original 35
+								{"NoBoostCombatEventMinFreighterBattleTime", "0.1"}, -- Original 55
+								{"NoBoostCombatEventTime", "0.1"}, -- Original 25
+								{"NormalModeTakeOffCostMultiplier", "0"}, -- Original 1
+								{"SurvivalTakeOffCostMultiplier", "0"}, -- Original 2
+								{"TakeOffCost", "0"}, -- Original 100
+								{"MiniWarpFuelTime", "999999"}, -- Original 0.5
+								{"MiniWarpChargeTime", "0.01"}, -- Original 2
+								{"MiniWarpSpeed", "99999"}, -- Original 30000
+								{"MiniWarpTime", "0.1"}, -- Original 10
+								{"MiniWarpCooldownTime", "0.1"}, -- Original 2
+								{"AnomalyStationMaxApproachSpeed", "6000"}, -- Original 60
+								{"BoostChargeRate", "100"}, -- Original 2.5
+								{"DockingRotateSpeed", "9"}, -- Original 1
+								{"DockingRotateSpeedVR", "9"}, -- Original 0.6
+								{"ShipEnterRange", "70"}, -- Original 7
+								{"ShipEnterSpeed", "99"}, -- Original 15
+								{"ShipEnterMinTime", "0.05"}, -- Original 0.5
+								{"LaunchThrustersMinimumSummonPercentage", "0"}, -- Original 25
+								{"LaunchThrustersSummonCostMultiplier", "0"}, -- Original 1	--_below here are better flight values, you can delete them if you don't like them not a part of the mod normally_--
 								{"GroundHeightSmoothTime",		"1000000"},-- Original v1.24 "0.4", 	Original v1.77 "0"  --underwater	--THIS WILL GENERATE A WARNING
 								{"GroundHeightSmoothTime",		"9000000"},-- Original v1.24 "0.4", 	Original v1.77 "0"  --underwater
-								{"LockTargetMinDistance", "1"},  --Original  0.5
-								{"NoBoostStationDistance", "0.1"},  --Original  5
-								{"NoBoostAnomalyDistance", "0.1"},  --Original  10
-								{"NoBoostSpaceAnomalyDistance", "0.1"},  --Original  10
-								{"NoBoostFreighterDistance", "0.1"},  --Original  10
-								{"NoBoostShipNearMinTime", "0.1"},  --Original  5
-								{"NoBoostShipLastHitTime", "0.1"},  --Original  10
-								{"NoBoostCombatEventMinBattleTime", "0.1"},  --Original  35
-								{"NoBoostCombatEventMinFreighterBattleTime", "0.1"},  --Original  55
-								{"NoBoostCombatEventTime", "0.1"},  --Original  25
-								{"NormalModeTakeOffCostMultiplier", "0"},  --Original  1
-								{"SurvivalTakeOffCostMultiplier", "0"},  --Original  2
-								{"TakeOffCost", "0"},  --Original  100
-								{"MiniWarpFuelTime", "999999"},  --Original  0.5
-								{"MiniWarpChargeTime", "0.1"},  --Original  2
-								{"MiniWarpSpeed", "99999"},  --Original  30000
-								{"MiniWarpTime", "0.1"},  --Original  10
-								{"MiniWarpCooldownTime",			"0.1"},	 --Original 2
 								{"CollisionSpeedDamageAmount",	"1"},		-- Original v1.24 "", 		Original v1.77 "1"		
-								{"CollisionDistanceGround",		"10"},		-- Original v1.24 "30", 		Original v1.77 "10"	
-								{"DockingRotateSpeed", "9"},  --Original  1
-								{"DockingRotateSpeedVR", "9"},  --Original  0.6
-								{"ShipEnterRange", "70"},	 --Original 7
-								{"ShipEnterSpeed", "99"},  --Original  15
-								{"ShipEnterMinTime", "0.05"},  --Original  0.5
-								{"LaunchThrustersMinimumSummonPercentage", "0"},  --Original  25
-								{"LaunchThrustersSummonCostMultiplier", "0"},  --Original  1
+								{"CollisionDistanceGround",		"10"},		-- Original v1.24 "30", 		Original v1.77 "10"										
+							}
+								
 							}
 						},
 						{
@@ -77,7 +83,7 @@ NMS_MOD_DEFINITION_CONTAINER =
 								{"DirectionBrake",			"1"},		-- Original v1.24 "2", 		Original v1.77 "2"							
 								{"OverspeedBrake",			"0.2"},		-- Original v1.24 "3", 		Original v1.77 "3"							
 								{"TurnBrakeMin",			"0.5"},		-- Original v1.24 "1", 		Original v1.77 "1"							
-								{"TurnBrakeMax",			"1"},		-- Original v1.24 "4", 		Original v1.77 "4"		
+								{"TurnBrakeMax",			"1"},		-- Original v1.24 "4", 		Original v1.77 "4"	
 							}
 						},
 						{
@@ -88,11 +94,11 @@ NMS_MOD_DEFINITION_CONTAINER =
 								{"MaxSpeed", "9999"},  -- Original 125
 								{"MinSpeed", "0"},  -- Original 1
 								{"BoostThrustForce", "200"},  -- Original 100
-								{"BoostMaxSpeed", "620"},  -- Original 155		--_below here are better flight values, you can delete them if you don't like them not a part of the mod normally_--
+								{"BoostMaxSpeed", "620"},  -- Original 155	--_below here are better flight values, you can delete them if you don't like them not a part of the mod normally_--
 								{"LowSpeedTurnDamper",		"1"},		-- Original v1.24 "0.3", 	Original v1.77 "0.25"	
 								{"TurnBrakeMin",			"0.2"},		-- Original v1.24 "0.8", 	Original v1.77 "2"	
 								{"TurnBrakeMax",			"5"},		-- Original v1.24 "6", 		Original v1.77 "3"	
-								{"TurnStrength",			"10"},		-- Original v1.24 "5", 		Original v1.77 "1"
+								{"TurnStrength",			"10"},		-- Original v1.24 "5", 		Original v1.77 "1"	
 							}
 						},
 						{
